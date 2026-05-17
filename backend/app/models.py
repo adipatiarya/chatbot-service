4ac4b71ffa9d6ae2815c9e2fb2016077ac33a5d7
RatmanDev <suratman@hype-plus.com>
2026-05-17 17:07:37 +0700

    add migration users

---
+++ backend/app/models.py
@@ -0,0 +1,25 @@
+import uuid
+from datetime import datetime, timezone
+from sqlalchemy import DateTime
+from pydantic import EmailStr
+from sqlmodel import SQLModel, Field
+
+def get_datetime_utc() -> datetime:
+    return datetime.now(timezone.utc)
+
+
+# Shared properties
+class UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    is_active: bool = True
+    is_superuser: bool = False
+    full_name: str | None = Field(default=None, max_length=255)
+    
+# Database model, database table inferred from class name
+class User(UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    hashed_password: str
+    created_at: datetime | None = Field(
+        default_factory=get_datetime_utc,
+        sa_type=DateTime(timezone=True),  # type: ignore
+    )
