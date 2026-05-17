6ac2315a705205f9cce2a6db8a6b3fe77f5cc34f
RatmanDev <suratman@hype-plus.com>
2026-05-17 15:11:47 +0700

    env config

--- backend/tests/scripts/test_test_pre_start.py
+++ backend/tests/scripts/test_test_pre_start.py
@@ -24,7 +24,7 @@ def test_init_successful_connection() -> None:
             init(engine_mock)
             connection_successful = True
         except Exception:
-             connection_successful = False
+            connection_successful = False
         assert connection_successful, (
             "The database connection should be successful and not raise an exception"
         )
