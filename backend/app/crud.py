57b7ef0bf5a1c646424b5301402889a5d458c91a
RatmanDev <suratman@hype-plus.com>
2026-05-18 20:36:59 +0700

    test update user

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,8 +1,8 @@
 
 from sqlmodel import Session, select
-from app.models import UserCreate, User
+from app.models import UserCreate, User, UserUpdate
 from app.core.security import get_password_hash, verify_password
-
+from typing import Any
 
 DUMMY_HASH = "$argon2id$v=19$m=65536,t=3,p=4$MjQyZWE1MzBjYjJlZTI0Yw$YTU4NGM5ZTZmYjE2NzZlZjY0ZWY3ZGRkY2U2OWFjNjk"
 
@@ -43,4 +43,17 @@ def authenticate(*, session: Session, email: str, password: str) -> User | None:
         session.commit()
         session.refresh()
 
+    return db_user
+
+def update_user(*, session: Session, db_user: User, user_in: UserUpdate) -> Any:
+    user_data = user_in.model_dump(exclude_unset=True)
+    extra_data = {}
+    if "password" in user_data:
+        password = user_data["password"]
+        hashed_password = get_password_hash(password)
+        extra_data["hashed_password"] = hashed_password
+    db_user.sqlmodel_update(user_data, update=extra_data)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
     return db_user
--- backend/tests/crud/test_user.py
+++ backend/tests/crud/test_user.py
@@ -1,5 +1,7 @@
+from fastapi.encoders import jsonable_encoder
 from tests.helpers.util import random_email, random_lower_string
-from app.models import UserCreate
+from app.models import UserCreate, User, UserUpdate
+from app.core.security import verify_password
 from sqlmodel import Session
 from app import crud
 
@@ -34,3 +36,42 @@ def test_check_user_is_active_inactive(sess: Session) -> None:
     user_in = UserCreate(email=email, password=password, is_active=False)
     user = crud.create_user(session=sess, user_create=user_in)
     assert user.is_active is False
+
+def test_check_user_is_superuser(sess: Session) -> None:
+    email = random_email()
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password, is_superuser=True)
+    user = crud.create_user(session=sess, user_create=user_in)
+    assert user.is_superuser is True
+
+def test_check_user_is_normal_user(sess: Session) -> None:
+    email = random_email()
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password)
+    user = crud.create_user(session=sess, user_create=user_in)
+    assert user.is_superuser is False
+
+def test_get_user(sess: Session) -> None:
+    email = random_email()
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password)
+    user = crud.create_user(session=sess, user_create=user_in)
+    user_2 = sess.get(User, user.id)
+    assert user_2
+    assert user.email == user_2.email
+    assert jsonable_encoder(user) == jsonable_encoder(user_2)
+
+def test_update_user(sess: Session) -> None :
+    email = random_email()
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password)
+    user = crud.create_user(session=sess, user_create=user_in)
+    new_password = random_lower_string()
+    user_in_update = UserUpdate(password=new_password, is_superuser=True)
+    if user.id is not None:
+        crud.update_user(session= sess, db_user=user, user_in=user_in_update)
+    user_2 = sess.get(User, user.id)
+    assert user_2
+    assert user.email == user_2.email
+    verified, _ = verify_password(plain_password= new_password, hashed_password=user_2.hashed_password)
+    assert verified
