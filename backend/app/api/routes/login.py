d805a35aabbe3d2585eac11b911da76cef04c0b6
RatmanDev <suratman@hype-plus.com>
2026-05-21 15:04:43 +0700

    test-password-reset

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 
 
