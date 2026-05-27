0c5461ab0a150a87a5da4ca5d2e9af79acb4533f
RatmanDev <suratman@hype-plus.com>
2026-05-27 22:08:35 +0700

    update

--- backend/tests/services/test_role_service.py
+++ backend/tests/services/test_role_service.py
@@ -1,8 +1,6 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.deps import get_role_service
-# from app.models.role import RoleCreate
-from app.models.permission import PermissionCreate
 from app.core.exception import DuplicateEntryError
 from app.models.role import RoleCreate
 
@@ -42,5 +40,26 @@ async def test_create_role_permission_with_not_exist_permissions(async_db: Async
 
     assert hasattr(role, "permissions")
     assert len(role.permissions) == 0
+
+@pytest.mark.asyncio
+async def test_create_role_permission_with_duplicate_roles(async_db: AsyncSession) -> None:
+    service = get_role_service(async_db)
+
+    role_name = 'supervisor'
+    
+    permission=['can_create_user','can_update_user','can_add_role'] #permission ini harus sudah ada
+
+    #await service.create_permissions(permission) #jangan di buat exist
+
+    data = RoleCreate(
+        name=role_name,
+        permission=permission
+    )
+    await service.create_or_update_role(data)
+    
+    #buat lagi. harus error
+    with pytest.raises(DuplicateEntryError):
+        await service.create_or_update_role(data)
+
 
 
